d3bc4e914877e243de2d95f46acc07077d57bbf9
Carlos Segura <cseguramail@gmail.com>
2017-09-07 16:44:37 +0200

    howto perform forced alignments

--- data_preparation/jobScreening_cvpr17/clean_transcriptions.sh
+++ data_preparation/jobScreening_cvpr17/clean_transcriptions.sh
@@ -2,6 +2,6 @@
 
 for fichero in $(ls transcripts)
   do
-    cat transcripts/$fichero |  python3 -c 'import sys; print( sys.stdin.read().lower() )' | sed 's@-@\ @g' | sed 's@–@\ @g' |  sed -e 's@\.\.\.@@g' -e 's@\[[a-z]* ..:..:..]@[vocalized-noise]@g' -e 's@\[[a-z0-9]* [a-z0-9]* ..:..:..]@[vocalized-noise]@g' -e 's@\.\.@.@g' -e 's@ \. @. @g' -e 's@\.@\ @g' | sed -e 's@_ @\ @g' -e 's@_$@@g' | sed -e 's@[,!\"?]@\ @g'  -e 's@\ \ @\ @g' > transcripts/${fichero}.clean
+    cat transcripts/$fichero |  python3 -c 'import sys; print( sys.stdin.read().lower() )' | sed 's@-@\ @g' | sed 's@–@\ @g' |  sed -e 's@\.\.\.@@g' -e 's@\[[^][]*\]@\ [vocalized-noise]\ @g' -e 's@\.\.@.@g' -e 's@ \. @. @g' -e 's@\.@\ @g' | sed -e 's@_ @\ @g' -e 's@_$@@g' | sed -e 's@[,!\"?]@\ @g'  -e 's@\ \ @\ @g' > transcripts/${fichero}.clean
     
 done
